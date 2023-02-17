d20ad042a29bfe3b56cd1805cc56350db36c212b
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-02-17 14:48:51 -0800

    alphabit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print letters from a to z
+ *
+ * Description: print the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  char x;
+  for (x = 'a; x <= 'z'; x++;)
+    {
+      putchar (x);
+    }
+  putchar('\n');
+}
