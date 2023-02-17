21dcb4561597354a781e90d52096882f5cd88809
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-02-17 14:54:07 -0800

    alphabit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,11 +12,11 @@
 
 int main(void)
 {
-  char x;
-  for (x = 'a'; x <= 'z'; x++)
-    {
-      putchar (x);
-    }
-  putchar('\n');
-  return (0);
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar (x);
+}
+putchar('\n');
+return (0);
 }
