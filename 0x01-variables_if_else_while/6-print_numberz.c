d02736e1cfe1ab61eb02968083510e91a15da7ac
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-02-17 15:34:27 -0800

    numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,15 +7,20 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int i;
-i = 0;
-while (i < 10)
-{
-putchar(i);
-i++;
-}
-putchar('\n');
-return (0);
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
 }
