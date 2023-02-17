55755747c531f6fe39da5d00917e877aae937361
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-02-17 15:28:40 -0800

    numbers 0-10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,16 +10,12 @@
 
 int main(void)
 {
-	int i;
-
-	i = 0;
-
-	for (i = 0; i < 10; i++)
-	{
-	  printf("%d", i);
-	}
-
-          putchar('\n');
-
-	return (0);
+int i;
+i = 0;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
 }
