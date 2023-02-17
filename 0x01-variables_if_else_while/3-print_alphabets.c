0d7c61f8cffccc3e66f6fc1c148084352cc00b5f
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-02-17 15:07:12 -0800

    2 way alphabeit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print letters from a to z
+ *
+ * Description: print the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar (x);
+}
+for (x 'A'; x <= 'Z'; x++)
+{
+putchar (x);
+}
+putchar('\n');
+return (0);
+}
