3eb56b40268cf7f9ca9e97a71393bdb238697ed5
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-02-21 08:34:25 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+  char c;
+  for (c = 'a';c <= 'z';c++)
+    {
+      putchar(c);
+    }
+  putchar('\n');
+}
