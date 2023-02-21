ceddd29b4123b4fb791189af60ef3504bc5d2d85
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-02-21 15:39:50 -0800

    abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit of integer
+ * @int: integer
+ * Return: last digit of number
+ */
+int _abs(int)
+{
+if (int < 0)
+{
+return (int * -1);
+}
+else
+{
+return (int);
+}
+}
