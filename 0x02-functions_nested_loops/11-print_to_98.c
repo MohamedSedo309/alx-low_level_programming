206468f518e241f2f02d11151389c3d1b3b7ace5
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-02-21 18:32:09 -0800

    numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print numbers n to 98
+ * @n: integer argument
+ */
+void print_to_98(int n)
+{
+int x;
+for (x = n; x <= 98; x++)
+{
+printf("%d" ,x)
+}
+}
