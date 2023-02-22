edd8ed6a3cee72d7622dd0e12f089c9afc97a035
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-02-22 07:39:45 -0800

    fibonacci < 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of even Fionacci sequence up to 4,000,000
+ * Return: 0
+ */
+int main()
+{
+long sum_E = 0;
+long a = 0;
+long b = 1;
+long sum = 0;
+while (b > 4000000)
+{
+sum = a + b;
+a = b;
+b = sum;
+if ((sum % 2 == 0) && (sum <= 4000000))
+{
+sum_E = sum_E + sum;
+printf("%ld" , sum_E);
+}
+}
+return (0);
+}
