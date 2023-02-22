b9372c6c231bed414f163c8096f56c0806fe1327
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-02-22 07:47:29 -0800

    fibonacci < 4000000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,7 +10,7 @@ long sum_E = 0;
 long a = 0;
 long b = 1;
 long sum = 0;
-while (b > 4000000)
+while (b < 4000000)
 {
 sum = a + b;
 a = b;
@@ -18,8 +18,8 @@ b = sum;
 if ((sum % 2 == 0) && (sum <= 4000000))
 {
 sum_E = sum_E + sum;
-printf("%ld" , sum_E);
 }
 }
+printf("%ld", sum_E);
 return (0);
 }
