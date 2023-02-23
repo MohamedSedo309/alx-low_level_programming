c616eb653f65a5cb4618b4e907c50ae55558cf08
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-02-23 17:05:48 +0200

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 
+/**
+* main - tests function that prints if integer is positive or negative
+* @i: the number to check
+* Return: 0
+*/
 void positive_or_negative(int i);
 
 #endif
