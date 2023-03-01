ba3e6ca2f6d531ea3f3dc9790066c9fdb7b9b0f8
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-03-01 13:38:10 -0800

    copy string for n chars

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,13 +8,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i = 0;
-    
-    for (i = 0; i < n; i++)
-    {
-    dest[i] = src[i];
-    }
-    dest[n] = '\0';
-    
-    return (dest);
+int i;
+for (i = 0; src[i] != '\0' && i < n ; i++)
+{
+dest[i] = src[i];
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
 }
