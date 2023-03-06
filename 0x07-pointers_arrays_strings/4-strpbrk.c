37b3bae0c3468a47f62c6a629f18e647a5403994
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-03-06 10:55:10 -0800

    Write a function that searches a string for any of a set of bytes.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#define NULL 0
+/**
+ * _strpbrk - return pointer to byte in s that matches a byte in accept
+ * @s: string to search
+ * @accept: string to compare
+ * Return: first occurence of string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i = 0;
+int j = 0;
+while (s[i] != '\0')
+{
+while (accept[j] != '\0')
+{
+if (s[i] == accept[j])
+{
+s = &s[i];
+return (s);
+}
+j++;
+}
+i++;
+}
+return (NULL);
+}
