741a808c9d0d2497e4a4d126cc90e494e70b8c61
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-03-09 01:44:29 +0200

    fixing errors

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,5 @@
 #include "main.h"
 /**
- * _sqrt_recursion - find natural square root of n
- * @n: n
- * Return: square root
- */
-
-int _sqrt_recursion(int n)
-{
-if (n < 0)
-{
-return (-1);
-}
-return (get_root(n, 0));
-}
-}
-
-/**
  * get_root - get square root of n
  * @n: n
  * @r: posible root
@@ -32,5 +16,20 @@ if (r * r == n)
 {
 return (r);
 }
-return (find_root(n, r + 1));
+return (get_root(n, r + 1));
+}
+
+/**
+ * _sqrt_recursion - find natural square root of n
+ * @n: n
+ * Return: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+return (find_root(n, 0));
 }
