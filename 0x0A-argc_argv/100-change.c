481fb319656c68753a2f8e8b632f926a28214e5b
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-03-12 22:43:32 +0200

    calculate the change of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,1 +1,42 @@
-#3
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - calculate the change of money
+ * @argc: two arguments
+ * @argv: args pointer
+ * Return: least number of coins, 0 if negative amount, 1 if amount not given
+ */
+
+int main(int argc, char *argv[])
+{
+	int n, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	n = atoi(argv[1]);
+
+	coins += n / 25;
+	n = n % 25;
+	coins += n / 10;
+	n = n % 10;
+	coins += n / 5;
+	n = n % 5;
+	coins += n / 2;
+	n = n % 2;
+	coins += n / 1;
+
+	printf("%d\n", coins);
+	return (0);
+}
