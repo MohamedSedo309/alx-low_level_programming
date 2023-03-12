97ddad7d0099cb0c4fe4b50204a210e670560e02
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-03-12 07:45:29 -0700

    Write a program that prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print all arguments in cmd
+ * @argc: arguments counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+ 
+int main(int argc, char *argv[])
+{
+int i;
+if (argc > 0)
+{
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+}
+return (0);
+}
