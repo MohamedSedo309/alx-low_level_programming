b6cec935dd2aa4e872b4c1581a2ce3ca3e26b492
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-03-12 17:41:24 +0200

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,17 +13,13 @@ int main(int argc, char *argv[])
 int i;
 int x;
 int y;
-if (argc == 3)
-{
-x = atoi(argv[1]);
-y = atoi(argv[2]);
-printf("%d\n", x * y);
-return (0);
-}
-else
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+printf("%d\n", x * y);
 return (0);
 }
