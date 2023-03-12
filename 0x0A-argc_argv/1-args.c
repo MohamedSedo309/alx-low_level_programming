048052051fdabfe630d4afa9433800d0c677ead9
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-03-12 16:53:05 +0200

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,14 +5,15 @@
  * main - print number of cmd arguments
  * @argc: arguments counter
  * @argv: pointer to array of arguments
+ *
  * Return: 0 on success
  */
- 
-int main(int argc, char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-if (argc > 0)
-{
-printf("%d\n", argc - 1);
-}
-return (0);
+    if (argc > 0)
+    {
+        printf("%d\n", argc - 1);
+    }
+
+    return (0);
 }
