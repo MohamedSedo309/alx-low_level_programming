877642c92b8f0f10cbfcfe48840d1bf8d27e101f
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-03-21 01:54:43 +0200

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
+
 #include <stdio.h>
-/*
+#include <stdlib.h>
+
+/**
 *struct dog - a new type of data
 *representing a dog
 *@name: name of dog
@@ -10,8 +13,8 @@
 */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 #endif
