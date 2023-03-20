91a72047014db9927e6f995bb5c2eaedb40fbed3
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-03-21 01:55:51 +0200

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,11 @@ char *name;
 float age;
 char *owner;
 };
+/**
+*dog_t - Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
