779bda067c0cf5862c4fdcddec952cf2f6250069
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-03-21 01:51:44 +0200

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
+
 #include <stdio.h>
+
 /*
 *struct dog - a new type of data
 *representing a dog
@@ -8,6 +10,7 @@
 *@age: age of dog
 *@owner: owner's name
 */
+
 struct dog
 {
 char *name;
