1af4d0a1f95db67f1d83f3e817e99b190da6a09f
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-03-23 15:35:44 -0700

    fixing errors

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,16 +1,15 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
  * sum_them_all - return sum of arguments
  * @n: number of integers
  * Return: sum
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
 va_list valist;
-unsigned int sum = 0, i = 0;
+unsigned int i = 0;
+int sum = 0;
 if (n == 0)
 {
 return (0);
