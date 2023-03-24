1a3ef5e76c97d3e53edf20ca98519a840c321097
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-03-24 16:45:02 -0700

    Write a program that prints the opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+/**
+ * main - This function prints the opcodes of its
+ * own main function
+ * in hexadecimal format
+ * @argc: The number of arguments
+ * @argv: Array of pointers to the arguments
+ *
+ * Return: 0 on success, 1 if argc != 2, 2 if negative
+ */
+int main(int argc, char *argv[])
+{
+        if (argc != 2)
+        {
+                printf("Error\n");
+                return (1);
+        }
+
+        int num_bytes = atoi(argv[1]);
+
+        if (num_bytes < 0)
+        {
+                printf("Error\n");
+                return (2);
+        }
+
+        unsigned char *ptr = (unsigned char *)main;
+
+        for (int i = 0; i < num_bytes; i++)
+        {
+                printf("%02x", *(ptr + i));
+        }
+
+        printf("\n");
+        return (0);
+}
