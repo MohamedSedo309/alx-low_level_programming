24be3d7da6d8d076e8451996a0aa07210ba592cd
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-03-30 12:54:22 -0700

    add new line

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,9 +13,9 @@ while (h != NULL)
 {
 if (h->str == NULL)
 {
-printf("[0] (nil)");
+printf("[0] (nil)\n");
 }
-printf("[%d] %s", h->len, h->str);
+printf("[%d] %s\n", h->len, h->str);
 h = h->next;
 i++;
 }
