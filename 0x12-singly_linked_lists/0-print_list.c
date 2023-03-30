380c395b1e18597cea5fe63106264aee327b586c
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-03-30 22:42:55 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,25 @@
 #include "lists.h"
-
 /**
 * print_list - prints all the elements of a list
-*
 * @h:pointer to single list
-*
 *Return: number of nodes
 */
-
 size_t print_list(const list_t *h)
 {
 const list_t *tmp = h;
 size_t i = 0;
-
 while (tmp != NULL)
 {
 if (tmp->str != NULL)
+{
 printf("[%d] %s\n", tmp->len, tmp->str);
+}
 else
+{
 printf("[0] (nil)\n");
+}
 i += 1;
 tmp = tmp->next;
 }
-
-return i;
+return (i);
 }
