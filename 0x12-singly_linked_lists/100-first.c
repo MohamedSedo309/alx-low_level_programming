e866da7946fae717c79adc6b4930c1d57649e7dc
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-03-31 09:15:28 -0700

    function before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
-* before_main - prints a line before main function
+* before_main - prints a line before
+* main function
 *
 *Return: void
 */
 void before_main(void) __attribute__ ((constructor));
-
 void before_main(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\nI bore 
+my house upon my back!\n");
 }
