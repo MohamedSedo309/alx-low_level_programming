d7120ed08153e37a2442d4d92a2da795036dc148
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-04-08 08:15:13 -0700

    fixing error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,7 +11,7 @@ unsigned int binary_to_uint(const char *b)
 {
 unsigned int result = 0;
 int len = 0;
-int i;
+int i = 0;
 if (b == NULL)
 {
 return (0);
@@ -20,7 +20,6 @@ while (b[i] != '\0')
 {
 len++;
 }
-i = 0;
 while (b[i])
 {
 if ((b[i] != '0') && (b[i] != '1'))
