91943426795b248c0fdcb39d1d6003eb0d1f94d9
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-04-09 09:06:51 -0700

    trying to flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*flip_bits - gets the number of bits to flip 
+* to change n to m
+*@n: first number
+*@m: second number
+*
+*Return: the number of flipped bits
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int flipped = n ^ m;
+int i = 0;
+while (flipped)
+{
+if (flipped & 1)
+{
+i++;
+}
+flipped >>= 1;
+}
+return (i);
+}
