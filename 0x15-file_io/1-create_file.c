bab88a6d726f11d8eccebc52b376af79e3b8f205
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-04-10 07:51:15 -0700

    Create a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - create a file with permissions
+ * rw------- and write in it
+ * @filename: file name
+ * @text_content: text to write in
+ * Return: 1 on success, -1 on error
+ */
+int create_file(const char *filename, char *text_content)
+{
+ssize_t file;
+ssize_t w;
+int str_len = 0;
+if (filename == NULL)
+{
+return (-1);
+}
+file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (file == -1)
+{
+return (-1);
+}
+if (text_content[0] == '\0')
+{
+close(file);
+return (1);
+}
+while (text_content[str_len] != '\0')
+{
+str_len++;
+}
+w = write(file, text_content, str_len);
+if (w == -1)
+{
+return (-1);
+}
+return (1);
+}
