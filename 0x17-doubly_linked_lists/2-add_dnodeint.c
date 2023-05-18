19bbe093f4badfd351a2e885f532bb1e3143a3e5
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-05-18 22:52:50 +0300

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /**
  * add_dnodeint - add new node to head
- * @h: list header
+ * @head: list header
+ * @n: new node int
  * Return: new node pointer
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
