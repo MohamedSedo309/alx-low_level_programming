44068abb14996037763b2560694fb721bd116bae
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2024-02-07 23:09:21 +0200

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,7 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-#include <stddef.h> // for size_t
+#include <stddef.h>
 
 // Function prototype for linear_search
 int linear_search(int *array, size_t size, int value);
